e90727ec1695c5d8d2fec1ef5b5f65f8f37e9348
Jordon D. Hemingway <jordonhemingway@gmail.com>
2017-05-02 18:49:42 -0400

    created init file

---
+++ fouriertransform/__init__.py
@@ -0,0 +1,35 @@
+'''
+Initializes the fouriertransform package.
+
+fouriertransform was created by:
+
+	Jordon D. Hemingway 
+	Harvard University
+	jordon_hemingway@fas.harvard.edu
+
+source code can be found at:
+	
+	https://github.com/FluvialSeds/fouriertransform
+
+documentation can be found at:
+
+	http://fouriertransform.readthedocs.io
+
+Version 0.0.1 is current as of 2 May 2017.
+
+'''
+
+from __future__ import(
+	division,
+	print_function,
+	)
+
+__version__ = '0.0.1'
+
+__docformat__ = 'restructuredtext en'
+
+
+#import formulatable classes
+from .formulatable import(
+	FormulaTable,
+	)
